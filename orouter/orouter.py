94c48b46d0bef077db896555d7c6d55357da5407
Andrew Pomorski <andrew.pomorski@awpcapital.net>
2023-04-04 20:05:15 +0800

    add order router python

---
+++ orouter/orouter.py
@@ -0,0 +1,16 @@
+class ORouter():
+    
+    def __init__(self):
+        pass
+
+    def parseOrder():
+        pass
+
+    def sendOrder():
+        pass
+
+    def logOrder():
+        pass
+
+    def sendOrderToPost():
+        pass
---
+++ shortvol/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import posbuild
+
+
+def main():
+    pbulder = posbuild.PosBuild()   
+    pass
+
+if __name__ == '__main__':
+    main()
